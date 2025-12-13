f16818c67d046d8993e59c673b73560a3882359f
bala-amypo <bala@amypo.in>
2025-12-13 10:04:55 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -0,0 +1,17 @@
+public class Vendor{
+    private long id;
+    private String vendorName;
+    private String contactEmail;
+    private String address;
+
+    public Vendor(long id,String vendorName,String contactEmail,String address){
+        this.id=id;
+        this.vendorName=vendorName;
+        this.contactEmail=contactEmail;
+        this.address=address;
+    }
+    public long getId{
+        return id;
+    }
+    public String 
+}
