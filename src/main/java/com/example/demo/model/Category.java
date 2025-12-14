11dd3f465642769ba470237e2787ae31a9811b34
bala-amypo <bala@amypo.in>
2025-12-14 04:23:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Category.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+public class Category{
+    private long id;
+    private String categoryName;
+    private String description;
+
+    public Category(long id,String categoryName,String description){
+        this.id=id;
+        this.categoryName=categoryName;
+        this.description=description;
+    }
+    public long getId(){
+        return id;
+    }
+    public S
+}
