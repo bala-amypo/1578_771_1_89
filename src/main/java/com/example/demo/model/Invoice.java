5154c38444befda6bd194661cff95ae41b620f46
bala-amypo <bala@amypo.in>
2025-12-14 04:21:09 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Invoice.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+public class Invoice{
+    private long id;
+    private String invoiceNumber;
+    private double amount;
+    private String description;
+    
+    public Invoice(long id,String invoiceNumber,double amount,String description){
+        this.id=id;
+        this.invoiceNumber=invoiceNumber;
+        this.amount=amount;
+        this.description=description;
+    }
+    public long getId(){
+        return id;
+    }
+    public String getInvoiceNumber(){
+        return invoiceNumber;
+    }
+    public double getAmount(){
+        return amount;
+    }
+    public String getDescription(){
+        return description;
+    }
+}
