fb5352c0e18794623701b651326769e4ecfc7577
bala-amypo <bala@amypo.in>
2025-12-17 11:41:29 +0000

    User commit

--- src/main/java/com/example/demo/model/Vendor.java
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -1,66 +1,66 @@
-package com.example.demo.model;
-import jakarta.persistence.*;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.validation.constraints.Email;
-import java.time.LocalDateTime;
-import jakarta.persistence.Entity;
-@Entity
-public class Vendor{
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private long id;
-    @Column(unique=true)
-    private String vendorName;
-    @Email
-    private String contactEmail;
-    private String address;
-    private LocalDateTime createdAt;
+// package com.example.demo.model;
+// import jakarta.persistence.*;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.validation.constraints.Email;
+// import java.time.LocalDateTime;
+// import jakarta.persistence.Entity;
+// @Entity
+// public class Vendor{
+//     @Id
+//     @GeneratedValue(strategy=GenerationType.IDENTITY)
+//     private long id;
+//     @Column(unique=true)
+//     private String vendorName;
+//     @Email
+//     private String contactEmail;
+//     private String address;
+//     private LocalDateTime createdAt;
 
-    public Vendor(){}
-    public Vendor(String vendorName,String contactEmail,String address,LocalDateTime createdAt){
-        this.vendorName=vendorName;
-        this.contactEmail=contactEmail;
-        this.address=address;
-        this.createdAt=createdAt;
-    }
+//     public Vendor(){}
+//     public Vendor(String vendorName,String contactEmail,String address,LocalDateTime createdAt){
+//         this.vendorName=vendorName;
+//         this.contactEmail=contactEmail;
+//         this.address=address;
+//         this.createdAt=createdAt;
+//     }
 
-    public long getId() {
-        return id;
-    }
+//     public long getId() {
+//         return id;
+//     }
 
-    public String getVendorName() {
-        return vendorName;
-    }
+//     public String getVendorName() {
+//         return vendorName;
+//     }
 
-    public String getContactEmail() {
-        return contactEmail;
-    }
+//     public String getContactEmail() {
+//         return contactEmail;
+//     }
 
-    public String getAddress() {
-        return address;
-    }
+//     public String getAddress() {
+//         return address;
+//     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+//     public void setId(long id) {
+//         this.id = id;
+//     }
 
-    public void setVendorName(String vendorName) {
-        this.vendorName = vendorName;
-    }
+//     public void setVendorName(String vendorName) {
+//         this.vendorName = vendorName;
+//     }
 
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
+//     public void setContactEmail(String contactEmail) {
+//         this.contactEmail = contactEmail;
+//     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-     public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
+//     public void setAddress(String address) {
+//         this.address = address;
+//     }
+//      public LocalDateTime getCreatedAt() {
+//         return createdAt;
+//     }
 
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt=createdAt;
-    }
-}
+//     public void setCreatedAt(LocalDateTime createdAt) {
+//         this.createdAt=createdAt;
+//     }
+// }
