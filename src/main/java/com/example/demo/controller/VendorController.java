7de5d454e43e730be4eef4e6aedf34a749288725
bala-amypo <bala@amypo.in>
2025-12-17 01:53:02 +0000

    User commit

--- src/main/java/com/example/demo/controller/VendorController.java
+++ src/main/java/com/example/demo/controller/VendorController.java
@@ -4,4 +4,29 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-i
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Vendor;
+import com.example.demo.service.VendorService;
+
+@RestController
+@RequestMapping("/api")
+public class VendorController{
+    @Autowired
+    VendorService vendorService;
+
+    @GetMapping("/vendors")
+    public List<Vendor>getAll(){
+        return vendorService.getAllVendors();
+    }
+    @PostMapping
+    public ResponseEntity<Vendor> createAll(@RequestBody Vendor vendor){
+        Vendor
+    }
+}
