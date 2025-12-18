34b9bd488043b601b75b3b66c9b4a1788a932865
bala-amypo <bala@amypo.in>
2025-12-18 10:28:45 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/InvoiceServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/InvoiceServiceImpl.java
@@ -1,47 +1,47 @@
-package com.example.demo.service.impl;
+// package com.example.demo.service.impl;
 
-import java.util.List;
-import java.util.Optional;
+// import java.util.List;
+// import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
  
-import com.example.demo.model.Invoice;
-import com.example.demo.repository.InvoiceRepository;
-import com.example.demo.model.User;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.model.Vendor;
-import com.example.demo.repository.VendorRepository;
-import com.example.demo.service.InvoiceService;
-@Service
-public class InvoiceServiceImpl implements InvoiceService{
-    @Autowired
-    InvoiceRepository invoiceRepository;
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    VendorRepository vendorRepository;
+// import com.example.demo.model.Invoice;
+// import com.example.demo.repository.InvoiceRepository;
+// import com.example.demo.model.User;
+// import com.example.demo.repository.UserRepository;
+// import com.example.demo.model.Vendor;
+// import com.example.demo.repository.VendorRepository;
+// import com.example.demo.service.InvoiceService;
+// @Service
+// public class InvoiceServiceImpl implements InvoiceService{
+//     @Autowired
+//     InvoiceRepository invoiceRepository;
+//     @Autowired
+//     UserRepository userRepository;
+//     @Autowired
+//     VendorRepository vendorRepository;
 
-    @Override
-    public Invoice uploadInvoice(Long userId,Long vendorId,Invoice invoice){
-       User user=userRepository.findById(userId).orElse(null);
-       Vendor vendor=vendorRepository.findById(vendorId).orElse(null);
-       invoice.setUploadedBy(user);
-       invoice.setVendor(vendor);
-       return invoiceRepository.save(invoice);
-    }
-    @Override
-    public Invoice categorizeInvoice(Long invoiceId){
-       Invoice invoice=invoiceRepository.findById(invoiceId).orElse(null);  
-       return invoiceRepository.save(invoice);
-    }
-    @Override
-    public List<Invoice> getInvoicesByUser(Long userId){
-       User user=userRepository.findById(userId).orElse(null);
-       return invoiceRepository.findByUploadedBy(user);
-    }
-    @Override
-    public Invoice getInvoice(Long invoiceId){
-      return invoiceRepository.findById(invoiceId).orElse(null);
-    }
-} 
+//     @Override
+//     public Invoice uploadInvoice(Long userId,Long vendorId,Invoice invoice){
+//        User user=userRepository.findById(userId).orElse(null);
+//        Vendor vendor=vendorRepository.findById(vendorId).orElse(null);
+//        invoice.setUploadedBy(user);
+//        invoice.setVendor(vendor);
+//        return invoiceRepository.save(invoice);
+//     }
+//     @Override
+//     public Invoice categorizeInvoice(Long invoiceId){
+//        Invoice invoice=invoiceRepository.findById(invoiceId).orElse(null);  
+//        return invoiceRepository.save(invoice);
+//     }
+//     @Override
+//     public List<Invoice> getInvoicesByUser(Long userId){
+//        User user=userRepository.findById(userId).orElse(null);
+//        return invoiceRepository.findByUploadedBy(user);
+//     }
+//     @Override
+//     public Invoice getInvoice(Long invoiceId){
+//       return invoiceRepository.findById(invoiceId).orElse(null);
+//     }
+// } 
