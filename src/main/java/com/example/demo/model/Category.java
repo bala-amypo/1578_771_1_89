bc039d0a4e1b363091f3702636b493440676ca46
bala-amypo <bala@amypo.in>
2025-12-18 06:09:26 +0000

    User commit

--- src/main/java/com/example/demo/model/Category.java
+++ src/main/java/com/example/demo/model/Category.java
@@ -1,45 +1,45 @@
-// package com.example.demo.model;
-// import jakarta.persistence.Column;
-// import jakarta.persistence.GeneratedValue;
-// import java.time.LocalDateTime;
-// @Entity
-// public class Category{
-//     @Id
-//      @GeneratedValue(strategy=GenerationType.IDENTITY)
-//     private long id;
-//     @Column(unique=true)
-//     private String categoryName;
-//     private String description;
-//     private LocalDateTime createdAt;
- 
-//     public Category(){}
-//     public Category(String categoryName,String description,LocalDateTime createdAt){
-//         this.categoryName=categoryName;
-//         this.description=description;
-//         this.createdAt=createdAt;
-//     }
-//     public long getId(){
-//         return id;
-//     }
-//     public String getCategoryName(){
-//         return categoryName;
-//     }
-//     public String getDescription(){
-//         return description;
-//     }
-//     public LocalDateTime getCreatedAt(){
-//         return createdAt;
-//     }
-//     public void setId(long id) {
-//         this.id = id;
-//     }
-//     public void setCategoryName(String categoryName) {
-//         this.categoryName = categoryName;
-//     }
-//     public void setDescription(String description) {
-//         this.description = description;
-//     }
-//     public void setCreatedAt(LocalDateTime createdAt){
-//         this.createdAt=createdAt;
-//     }
-// }
+package com.example.demo.model;
+import jakarta.persistence.*;
+import jakarta.persistence.GeneratedValue;
+import java.time.LocalDateTime;
+@Entity
+public class Category{
+    @Id
+     @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    @Column(unique=true)
+    private String categoryName;
+    private String description;
+    private LocalDateTime createdAt;
+    
+    public Category(){}
+    public Category(String categoryName,String description,LocalDateTime createdAt){
+        this.categoryName=categoryName;
+        this.description=description;
+        this.createdAt=createdAt;
+    }
+    public long getId(){
+        return id;
+    }
+    public String getCategoryName(){
+        return categoryName;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt=createdAt;
+    }
+}
--- src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImpl.java
@@ -1,28 +1,28 @@
-// package com.example.demo.service.impl;
+package com.example.demo.service.impl;
 
-// import java.util.List;
-// import java.util.Optional;
+import java.util.List;
+import java.util.Optional;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// import com.example.demo.model.Category;
-// import com.example.demo.repository.CategoryRepository;
-// import com.example.demo.service.CategoryService;
-// @Service
-// public class CategoryServiceImpl implements CategoryService{
-//     @Autowired
-//     CategoryRepository categoryRepository;
-//     @Override
-//     public Category createCategory(Category category){
-//        return categoryRepository.save(category);
-//     }
-//     @Override
-//     public Category getCategory(Long id){
-//        Optional<Category> optionalCategory=categoryRepository.findById(id);
-//        return optionalCategory.orElse(other: null);
-//     }
-//     public List<Category> getAllCategories(){
-//        return categoryRepository.findAll();
-//     }
-// }
+import com.example.demo.model.Category;
+import com.example.demo.repository.CategoryRepository;
+import com.example.demo.service.CategoryService;
+@Service
+public class CategoryServiceImpl implements CategoryService{
+    @Autowired
+    CategoryRepository categoryRepository;
+    @Override
+    public Category createCategory(Category category){
+       return categoryRepository.save(category);
+    }
+    @Override
+    public Category getCategory(Long id){
+       Optional<Category> optionalCategory=categoryRepository.findById(id);
+       return optionalCategory.orElse(other: null);
+    }
+    public List<Category> getAllCategories(){
+       return categoryRepository.findAll();
+    }
+}
