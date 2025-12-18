7bb44f497141530aeb70794ec3bc529aa4a18d2c
bala-amypo <bala@amypo.in>
2025-12-18 09:29:37 +0000

    User commit

--- src/main/java/com/example/demo/model/Invoice.java
+++ src/main/java/com/example/demo/model/Invoice.java
@@ -1,10 +1,14 @@
 package com.example.demo.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Positive;
 import jakarta.persistence.GeneratedValue;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import com.example.demo.model.User;
+import com.example.demo.model.Category;
 @Entity
-@Table(name="invoices",uniqueConstraints=@UniqueConstraint (columnNames={"vendorId","invoiceNumber"})
+@Table(name="invoices",uniqueConstraints=@UniqueConstraint (columnNames={"vendorId","invoiceNumber"}))
 public class Invoice{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -12,13 +16,12 @@ public class Invoice{
     @ManyToOne(optional=false)
     @JoinColumn(name="vendorId")
     private Vendor vendor;
-    @Column(unique=true)
+    @Column(unique=true,nullable=false)
     private String invoiceNumber;
     @Positive
     private Double amount;
     private LocalDate invoiceDate;
     private String description;
-    @Column(nullable=true)
     @ManyToOne
     @JoinColumn(name="categoryId")
     private Category category;
@@ -43,6 +46,12 @@ public class Invoice{
         this.uploadedBy=uploadedBy;
         this.uploadedAt=uploadedAt;
     }
+    public Vendor getVendor(){
+    return vendor;
+    }
+    public void setVendor(Vendor vendor){
+    this.vendor=vendor;
+    }
     public long getInvoiceId(){
         return invoiceId;
     }
@@ -54,6 +63,30 @@ public class Invoice{
     }
     public String getDescription() {
         return description;
+    }
+    public LocalDate getInvoiceDate(){
+        return invoiceDate;
+    }
+    public Category getCategory(){
+        return category;
+    }
+    public User getUploadedBy(){
+        return uploadedBy;
+    }
+    public LocalDateTime getUploadedAt(){
+        return uploadedAt;
+    }
+    public void setUploadedAt(LocalDateTime uploadedAt){
+    this.uploadedAt=uploadedAt;
+    }
+    public void setUploadedBy(User uploadedBy){
+        this.uploadedBy=uploadedBy;
+    }
+    public void setCategory(Category category){
+        this.category=category;
+    }
+    public void setInvoiceDate(LocalDate invoiceDate){
+        this.invoiceDate=invoiceDate;
     }
     public void setInvoiceId(long invoiceId) {
         this.invoiceId = invoiceId;
--- src/main/java/com/example/demo/repository/InvoiceRepository.java
+++ src/main/java/com/example/demo/repository/InvoiceRepository.java
@@ -1,11 +1,12 @@
-// package com.example.demo.repository;
+package com.example.demo.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.model.User;
+import java.util.List;
+import com.example.demo.model.Invoice;
 
-// import com.example.demo.model.Invoice;
-
-// @Repository
-// public interface InvoiceRepository extends JpaRepository<Invoice,Long>{
-
-// }
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice,Long>{
+    List<Invoice> findByUploadedBy(User user);
+} 
