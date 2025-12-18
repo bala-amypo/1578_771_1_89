0ab76633e519bc285accd0edd7208e40607ce645
bala-amypo <bala@amypo.in>
2025-12-18 05:04:26 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -7,5 +7,5 @@ import com.example.demo.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>{
-     Optional<User> findByEmail(String email);
+     boolean existsByEmail(String email);
 }
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -13,9 +13,19 @@ import com.example.demo.service.UserService;
 public class UserServiceImpl implements UserService{
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
     @Override
-    public User registerUser(User user){
-       return userRepository.save(user);
+    public User registerUser(User user) {
+
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new IllegalArgumentException(
+                    "Email already in use: " + user.getEmail()
+            );
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
     }
     @Override
     public User findByEmail(String email){
