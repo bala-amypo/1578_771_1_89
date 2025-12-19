da68a7971ee4f4db7b0d577a684a2701ba327f2f
bala-amypo <bala@amypo.in>
2025-12-19 05:31:00 +0000

    User commit

--- src/main/java/com/example/demo/model/Invoice.java
+++ src/main/java/com/example/demo/model/Invoice.java
@@ -1,6 +1,6 @@
 package com.example.demo.model;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import jakarta.validation.constraints.Positive;
 import jakarta.persistence.GeneratedValue;
 import java.time.LocalDate;
