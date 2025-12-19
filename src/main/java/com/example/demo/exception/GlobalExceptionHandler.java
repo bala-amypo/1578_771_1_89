4655c4d8f0c12f7b803a2d9e5354e2884cb3f0ad
bala-amypo <bala@amypo.in>
2025-12-19 01:04:58 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -5,4 +5,18 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.R
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler{
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Object> handleNotFound(
+        ResourceNotFoundException ex,
+        WebRequest req
+    ){
+        return ResponseEntity.status(404).body(Map.of(
+            "timestamp",LocalDateTime.no
+        )
+    }
+}
