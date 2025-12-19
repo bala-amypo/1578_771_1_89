1d13dba89c2033bd0d9216b313707b271463420e
bala-amypo <bala@amypo.in>
2025-12-19 01:01:58 +0000

    User commit

---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.example.demo.exception;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.R
