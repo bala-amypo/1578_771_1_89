1a99be169736f34b4932c2b9bd36c8242665c7a1
bala-amypo <bala@amypo.in>
2025-12-20 10:40:19 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VendorServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VendorServiceImpl.java
@@ -1,41 +1,41 @@
-// package com.example.demo.service.impl;
+package com.example.demo.service.impl;
 
-// import java.util.List;
-// import java.util.Optional;
-// import java.util.regex.Pattern;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-// import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-// import com.example.demo.model.Vendor;
-// import com.example.demo.repository.VendorRepository;
-// import com.example.demo.service.VendorService;
-// import com.example.demo.exception.ResourceNotFoundException;
-// import com.example.demo.exception.IllegalArgumentException;
+import com.example.demo.model.Vendor;
+import com.example.demo.repository.VendorRepository;
+import com.example.demo.service.VendorService;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.exception.IllegalArgumentException;
 
-// @Service
-// @Transactional
-// public class VendorServiceImpl implements VendorService{
-//    private VendorRepository vendorRepository;
-//    public VendorServiceImpl(VendorRepository vendorRepository){
-//         this.vendorRepository=vendorRepository;
-//    }
-//     @Override
-//     public Vendor createVendor(Vendor vendor){
-//       if(vendorRepository.existsByName(vendor.getVendorName())){
-//       throw new ResourceNotFoundException("Vendor name already exixts");
-//       }
-//       if(vendor.getContactEmail()==null || Pattern.matches("^[A-Za-z0-9+_.-]+@(.+)$",vendor.getContactEmail())){
-//          throw new IllegalArgumentException("Invalid contact email format");
-//       }
-//       return vendorRepository.save(vendor);
-//     }
-//     @Override
-//     public Vendor getVendor(Long vendorId){
-//        return vendorRepository.findById(vendorId).orElseThrow(()->new ResourceNotFoundException("Vendor not found with id: "+ vendorId));
-//     }
-//     public List<Vendor> getAllVendors(){
-//        return vendorRepository.findAll();
-//     }
-// }
+@Service
+@Transactional
+public class VendorServiceImpl implements VendorService{
+   private VendorRepository vendorRepository;
+   public VendorServiceImpl(VendorRepository vendorRepository){
+        this.vendorRepository=vendorRepository;
+   }
+    @Override
+    public Vendor createVendor(Vendor vendor){
+      if(vendorRepository.existsByName(vendor.getVendorName())){
+      throw new ResourceNotFoundException("Vendor name already exixts");
+      }
+      if(vendor.getContactEmail()==null || Pattern.matches("^[A-Za-z0-9+_.-]+@(.+)$",vendor.getContactEmail())){
+         throw new IllegalArgumentException("Invalid contact email format");
+      }
+      return vendorRepository.save(vendor);
+    }
+    @Override
+    public Vendor getVendor(Long id){
+       return vendorRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Vendor not found"));
+    }
+    public List<Vendor> getAllVendors(){
+       return vendorRepository.findAll();
+    }
+}
