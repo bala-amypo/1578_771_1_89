0898907f7b8c4f6a521a7a554a1d22659272a765
bala-amypo <bala@amypo.in>
2025-12-21 05:41:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/InvoiceRepository.java
+++ src/main/java/com/example/demo/repository/InvoiceRepository.java
@@ -1,13 +1,13 @@
-// package com.example.demo.repository;
+package com.example.demo.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-// import com.example.demo.model.User;
-// import java.util.List;
-// import com.example.demo.model.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.model.User;
+import java.util.List;
+import com.example.demo.model.Invoice;
 
-// @Repository
-// public interface InvoiceRepository extends JpaRepository<Invoice,Long>{
-//     List<Invoice> findByUploadedBy(User user);
-//     List<Invoice> findByAmountGreaterThanHql(Double amount);
-// } 
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice,Long>{
+    List<Invoice> findByUploadedBy(User user);
+    // List<Invoice> findByAmountGreaterThanHql(Double amount);
+} 
--- src/main/java/com/example/demo/service/impl/InvoiceServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/InvoiceServiceImpl.java
@@ -1,53 +1,53 @@
-// package com.example.demo.service.impl;
+package com.example.demo.service.impl;
 
-// import java.util.List;
-// import java.util.Optional;
+import java.util.List;
+import java.util.Optional;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
  
-// import com.example.demo.model.Invoice;
-// import com.example.demo.repository.InvoiceRepository;
-// import com.example.demo.model.User;
-// import com.example.demo.repository.UserRepository;
-// import com.example.demo.model.Vendor;
-// import com.example.demo.repository.VendorRepository;
-// import com.example.demo.service.InvoiceService;
-// import com.example.demo.exception.ResourceNotFoundException;
-// import org.springframework.transaction.annotation.Transactional;
+import com.example.demo.model.Invoice;
+import com.example.demo.repository.InvoiceRepository;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.model.Vendor;
+import com.example.demo.repository.VendorRepository;
+import com.example.demo.service.InvoiceService;
+import com.example.demo.exception.ResourceNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
 
-// @Service
-// @Transactional
-// public class InvoiceServiceImpl implements InvoiceService{
-//     private InvoiceRepository invoiceRepository;
-//     private UserRepository userRepository;
-//     private VendorRepository vendorRepository;
-//     public InvoiceServiceImpl(InvoiceRepository invoiceRepository,UserRepository userRepository,VendorRepository vendorRepository){
-//         this.invoiceRepository=invoiceRepository;
-//         this.userRepository=userRepository;
-//         this.vendorRepository=vendorRepository;
-//     }
+@Service
+@Transactional
+public class InvoiceServiceImpl implements InvoiceService{
+    private InvoiceRepository invoiceRepository;
+    private UserRepository userRepository;
+    private VendorRepository vendorRepository;
+    public InvoiceServiceImpl(InvoiceRepository invoiceRepository,UserRepository userRepository,VendorRepository vendorRepository){
+        this.invoiceRepository=invoiceRepository;
+        this.userRepository=userRepository;
+        this.vendorRepository=vendorRepository;
+    }
 
-//     @Override
-//     public Invoice uploadInvoice(Long userId,Long vendorId,Invoice invoice){
-//        User user=userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User not found"));
-//        Vendor vendor=vendorRepository.findById(vendorId).orElseThrow(()->new ResourceNotFoundException("Vendor not found"));
-//        invoice.setUploadedBy(user);
-//        invoice.setVendor(vendor);
-//        return invoiceRepository.save(invoice);
-//     }
-//     @Override
-//     public Invoice categorizeInvoice(Long invoiceId){
-//        Invoice invoice=invoiceRepository.findById(invoiceId).orElseThrow(()->new ResourceNotFoundException("Invoice not found"));
-//        return invoiceRepository.save(invoice);
-//     }
-//     @Override
-//     public List<Invoice> getInvoicesByUser(Long userId){
-//        User user=userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User not found"));
-//        return invoiceRepository.findByUploadedBy(user);
-//     }
-//     @Override
-//     public Invoice getInvoice(Long invoiceId){
-//       return invoiceRepository.findById(invoiceId).orElseThrow(()->new ResourceNotFoundException("Invoice not found"));
-//     }
-// } 
+    @Override
+    public Invoice uploadInvoice(Long userId,Long vendorId,Invoice invoice){
+       User user=userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User not found"));
+       Vendor vendor=vendorRepository.findById(vendorId).orElseThrow(()->new ResourceNotFoundException("Vendor not found"));
+       invoice.setUploadedBy(user);
+       invoice.setVendor(vendor);
+       return invoiceRepository.save(invoice);
+    }
+    @Override
+    public Invoice categorizeInvoice(Long invoiceId){
+       Invoice invoice=invoiceRepository.findById(invoiceId).orElseThrow(()->new ResourceNotFoundException("Invoice not found"));
+       return invoiceRepository.save(invoice);
+    }
+    @Override
+    public List<Invoice> getInvoicesByUser(Long userId){
+       User user=userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User not found"));
+       return invoiceRepository.findByUploadedBy(user);
+    }
+    @Override
+    public Invoice getInvoice(Long invoiceId){
+      return invoiceRepository.findById(invoiceId).orElseThrow(()->new ResourceNotFoundException("Invoice not found"));
+    }
+} 
