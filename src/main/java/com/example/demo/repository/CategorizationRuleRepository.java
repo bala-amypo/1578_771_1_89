9e962830d8658606d3a5fb5a94ff6ca31bee39d1
bala-amypo <bala@amypo.in>
2025-12-23 17:10:14 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
+++ src/main/java/com/example/demo/repository/CategorizationRuleRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import com.example.demo.model.CategorizationRule;
+import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface CategorizationRuleRepository extends JpaRepository<CategorizationRule, Long> {
-    List<CategorizationRule> findByCategory_Id(Long categoryId);
+    // List<CategorizationRule> findByCategory_Id(Long categoryId);
+    @Query(""" SELECT r FROM CategorizationRule r WHERE LOWER( :description) LIKE LOWER(CONCAT('%, r.keyword,'%')) ORDER BY r.priority DESC """)
+    List<Cat
 }
