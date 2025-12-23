bd1aa9ca4f831ca917f20bbdc4563484456988a2
bala-amypo <bala@amypo.in>
2025-12-23 17:29:26 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategorizationRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CategorizationRuleServiceImpl.java
@@ -14,6 +14,7 @@ import com.example.demo.repository.CategorizationRuleRepository;
 import com.example.demo.service.CategorizationRuleService;
 import com.example.demo.repository.CategoryRepository;
 import com.example.demo.exception.ResourceNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
