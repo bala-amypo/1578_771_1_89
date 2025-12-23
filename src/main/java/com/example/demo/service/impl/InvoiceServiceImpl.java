5d10a61fc5ccf993f7011ef5407bcf69564a958d
bala-amypo <bala@amypo.in>
2025-12-23 17:25:15 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/InvoiceServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/InvoiceServiceImpl.java
@@ -40,6 +40,9 @@ public class InvoiceServiceImpl implements InvoiceService{
     public Invoice uploadInvoice(Long userId,Long vendorId,Invoice invoice){
        User user=userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User not found"));
        Vendor vendor=vendorRepository.findById(vendorId).orElseThrow(()->new ResourceNotFoundException("Vendor not found"));
+       if(invoice.getAmount()==null || invoice.getAmount()<=0){
+       throw IllegalArgumentException("Invoice amount must be greater than zero");
+       }
        invoice.setUploadedBy(user);
        invoice.setVendor(vendor);
        invoice.setCategory(null);
