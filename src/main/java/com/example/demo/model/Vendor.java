7b6e15511d686e9959891b2a1b01734496a7d86f
bala-amypo <bala@amypo.in>
2025-12-25 07:08:51 +0000

    User commit

--- src/main/java/com/example/demo/model/Vendor.java
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -46,7 +46,7 @@ public class Vendor {
     }
 
     public Set<User> getUsers() {
-        return users;
+        return null;
     }
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
